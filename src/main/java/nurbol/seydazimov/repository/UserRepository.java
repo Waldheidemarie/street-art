41b376d5c3d5c83691e55a9ae2c230ba2ad9b85c
Nurbol Seidazimov <seydazimov.nurbol@gmail.com>
2020-02-02 12:45:10 +0600

    S_ART-1000 Implement user register request and response

--- src/main/java/nurbol/seydazimov/repository/UserRepository.java
+++ src/main/java/nurbol/seydazimov/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package nurbol.seydazimov.repository;
 
-import nurbol.seydazimov.domain.User;
+import nurbol.seydazimov.domain.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends CrudRepository<UserEntity, UUID> {
 }
